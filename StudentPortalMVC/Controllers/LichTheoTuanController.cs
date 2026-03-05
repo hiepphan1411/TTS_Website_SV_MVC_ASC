e7d96efac4cbde7f3510ed0c2a5f6abb5ae289ff
Hồ Quang Nhân <hqn19072004@gmail.com>
2026-03-05 09:50:13 +0700

    lich-theo-tuan

---
+++ StudentPortalMVC/Controllers/LichTheoTuanController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudentPortalMVC.Controllers
+{
+    public class LichTheoTuanController : Controller
+    {
+        // GET: LichTheoTuan
+        [ActionName("Lich-theo-tuan")]
+        public ActionResult LichTheoTuan()
+        {
+            return View("LichTheoTuan");
+        }
+
+        // URL: /LichTheoTuan/GetData
+        public ActionResult GetData()
+        {
+            string path = Server.MapPath("~/App_Data/lich-theo-tuan.json");
+            string json = System.IO.File.ReadAllText(path);
+
+            return Content(json, "application/json");
+        }
+    }
+}
